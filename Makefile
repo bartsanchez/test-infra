f40c715fd7266b5f0af2d7605472b8e2f6da766f
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2024-02-12 20:39:32 +0100

    NEW: Allow VM user for ssh conneciton as envvar

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: deploy
 
+ANSIBLE_USER ?= root
+export ANSIBLE_USER
+
 deploy:
 	docker compose -f deployment/docker-compose.yml build
 	docker compose -f deployment/docker-compose.yml run --rm ansible-deployment
--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,4 +9,4 @@ ANSIBLE_PATH="$CURRENT_PATH/ansible"
 TARGET="${ANSIBLE_PATH}/production"
 MASTER_PLAYBOOK="${ANSIBLE_PATH}/site.yml"
 
-ansible-playbook -i ${TARGET} ${MASTER_PLAYBOOK} -e "ansible_host=${ANSIBLE_HOST}"
+ansible-playbook -i ${TARGET} ${MASTER_PLAYBOOK} -u "${ANSIBLE_USER}" -e "ansible_host=${ANSIBLE_HOST}"
