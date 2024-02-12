da0acfb50e0d47fc31a8a7dde29c1eb92a291d44
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2024-02-12 20:04:11 +0100

    NEW: Allow vm target IP as envvar

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ```sh
 ssh-add rsa
 ```
+
+## Prepare environment
+
+Set ANSIBLE_HOST envvar with the target virtual machine IP. For example:
+
+```sh
+export ANSIBLE_HOST="1.2.3.4"
+```
--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,4 +9,4 @@ ANSIBLE_PATH="$CURRENT_PATH/ansible"
 TARGET="${ANSIBLE_PATH}/production"
 MASTER_PLAYBOOK="${ANSIBLE_PATH}/site.yml"
 
-ansible-playbook -i ${TARGET} ${MASTER_PLAYBOOK}
+ansible-playbook -i ${TARGET} ${MASTER_PLAYBOOK} -e "ansible_host=${ANSIBLE_HOST}"
